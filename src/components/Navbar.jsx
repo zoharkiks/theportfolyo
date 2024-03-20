7d8b705f75af3f99a0d833ca17ddf3a07a8dfe9c
Zohar Williams <zoharkiks@gmail.com>
2024-03-21 01:31:40 +0530

    Initial Styles Setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="fixed flex justify-between w-full p-5 ">
+      <span className="text-xl font-medium">J.Doe</span>
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,14 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      background: "var(--color-background)",
+      primary: "var(--color-primary)",
+      accent: "var(--color-accent)",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        gilroy: ["gilroy"],
       },
     },
   },
